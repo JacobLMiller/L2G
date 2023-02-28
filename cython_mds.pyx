fc8a42c2ada8c8b141a1251517974f783dc2fe6e
Jacob Lee Miller <jacobmiller1@cs.arizona.edu>
2023-02-27 18:13:22 -0700

    attempted more changes

--- cython_mds.pyx
+++ cython_mds.pyx
@@ -4,12 +4,13 @@ cimport numpy as np
 cimport cython
 
 from libc.stdlib cimport rand, malloc, free
+from libc.math cimport sqrt
 
 cdef struct Pair:
     int u 
     int v 
-    float dist
-    float weight
+    double d
+    double w
 
 cdef struct Vertex:
     float x
@@ -31,17 +32,42 @@ cdef  Pair * fisheryates(Pair *arr, int n):
 cdef float get_step_size():
     return 0.01
 
-cdef double sgd(double *X, Pair *pairs, int n_vert,int n_pairs):
+cdef double *sgd(double *X, Pair *pairs, int n_vert,int n_pairs):
     cdef int n_iter = 10
+    cdef int epoch, p
+    cdef Pair pair
+    cdef int i, j 
+    cdef double d_ij, w_ij, mu, step, dx, dy, mag, r, r_x, r_y
     
     for epoch in range(n_iter):
+        step = 0.01
         for p in range(n_pairs):
-            Pair pair = pairs[p]
-            cdef int i = pair.u 
-            cdef int j = pair.v 
-            cdef float dist = pair.dist
-            cdef float weight = pair.weight
-    return 0.0
+            pair = pairs[p]
+            i = pair.u
+            j = pair.v 
+            d_ij = pair.d 
+            w_ij = pair.w 
+
+            mu = step * w_ij 
+            if mu > 1: mu = 1
+
+            dx = X[i*2] - X[j*2]
+            dy = X[i*2+1] - X[j*2+1]
+            mag = sqrt(dx*dx + dy*dy)
+
+            r = (mu * (mag-d_ij)) / (2*mag)
+            r_x = r*dx 
+            r_y = r*dy
+
+            X[i*2] -= r_x 
+            X[i*2+1] -= r_y 
+            X[j*2] += r_x 
+            X[j*2+1] += r_y
+
+
+
+            
+    return X
 
 
 cdef Pair *dummy_pair(int n_vertices):
@@ -51,12 +77,12 @@ cdef Pair *dummy_pair(int n_vertices):
     )
     cdef int u,v, i
     i = 0
-    for u in range(n):
+    for u in range(n_vertices):
         for v in range(u):
             pairs[i].u = u
             pairs[i].v = v 
-            pairs[i].dist = 1.0
-            pairs[i].weight = 1.0
+            pairs[i].d = 1.0
+            pairs[i].w = 1.0
             i += 1
     return pairs
 
@@ -81,19 +107,19 @@ def shuffle(d):
     
 
 
-def get_pair():
-    ptr = Pair(1,2,3.0)
-    return ptr
+
+
+
+
 
 def test_sgd():
     cdef int n = 50
     cdef int i
-    cdef double *pos = <double *> malloc(n * sizeof(double))
-    for i in range(n):
+    cdef double *pos = <double *> malloc(2 * n * sizeof(double))
+    for i in range(2*n):
         pos[i] = i
     cdef Pair *pairs = dummy_pair(n)
-    cdef double s
-    s = sgd(pos,n)
+    pos = sgd(pos, pairs, n, (n*(n-1))/2)
+    cdef double[:] view1 = pos 
     free(pos)
-    print(s)
-    return s
+    return view1
